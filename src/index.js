3d9c8e9cf243085a094b3b6cd16ccdf10191904f
elijahcraig45 <elijahcraig45@gmail.com>
2023-06-01 13:03:03 -0400

    reset homepage route

--- src/index.js
+++ src/index.js
@@ -21,7 +21,8 @@ root.render(
     <Router>
       <Navbar/>
       <Routes>
-        <Route path="/" element={<App />} />
+      <Route path="/" element={<App />} />
+        <Route path="/reactBlog" element={<App />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/about" element={<About />} />
         <Route path='/blog' element={<BlogList />} />
